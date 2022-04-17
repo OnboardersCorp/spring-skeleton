c8abbafa0e264707de170d1d870af66e83ed79cd
Seonyul <subid1@naver.com>
2022-04-18 08:55:53 +0900

    feat: 프로젝트 생성

---
+++ src/main/kotlin/com/tastygear/springskeleton/SpringSkeletonApplication.kt
@@ -0,0 +1,11 @@
+package com.tastygear.springskeleton
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSkeletonApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringSkeletonApplication>(*args)
+}
---
+++ src/test/kotlin/com/tastygear/springskeleton/SpringSkeletonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tastygear.springskeleton
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSkeletonApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
